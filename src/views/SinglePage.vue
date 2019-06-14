1f2af3e4d8839fe88a83220b8b1f13aa6184db9c
b-vera <bastian.vera.p@usach.cl>
2019-06-14 19:15:25 -0400

    CHG: Se agrega AboutUs.vue

--- src/views/SinglePage.vue
+++ src/views/SinglePage.vue
@@ -2,6 +2,7 @@
 	<div>
 		<Header></Header>
     <Index></Index>
+    <AboutUs></AboutUs>
 		<Contact></Contact>
     <Experience></Experience>
 		<News></News>
@@ -17,6 +18,7 @@ import Index from '@/sections/Index.vue';
 import News from '@/sections/News.vue';
 import Contact from '@/sections/Contact.vue';
 import Experience from '@/sections/Experience.vue';
+import AboutUs from '@/sections/AboutUs.vue';
 
 export default {
   name: 'home',
@@ -26,6 +28,7 @@ export default {
     News,
     Contact,
     Experience,
+    AboutUs,
     Footer
   }
 }
