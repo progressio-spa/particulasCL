a356f24e82d07381af0036baa68cba395e85ec49
b-vera <bastian.vera.p@usach.cl>
2019-06-16 19:36:53 -0400

    ADD: se agrega vista banner

---
+++ src/sections/Banner.vue
@@ -0,0 +1,60 @@
+<template>
+    <section class="hero is-large">
+        <div class="hero-body">
+            <div class="columns" id="columns-header">
+                <div class="column is-full">
+                    <div class="header">
+                        <p class="title">Aqui el banner</p>
+                    </div>
+                    <div class="description">
+                        <p>relleno
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+    .hero-body {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        background-color: white;
+        padding-left: 8vw;
+        padding-right: 8vw;
+    }
+
+    .title {
+        color: #47525E;
+    }
+
+    .header {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        padding-bottom: 5vh;
+    }
+
+    .description{
+        color: #8190A5;
+    }
+
+    .aboutUs-body {
+        height: 100%;
+        color: #8190A5;
+        text-align: justify;
+    }
+
+    .color {
+        width: 100%;
+        height: 100%;
+        background-color: rgb(226, 226, 226);
+    }
+
+    #columns-header {
+        display: block;
+    }
+</style>
