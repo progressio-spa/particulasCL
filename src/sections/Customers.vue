c8d648bc6fef5a3d4109c2750c4ff1d1666857db
b-vera <bastian.vera.p@usach.cl>
2019-08-08 18:34:09 -0400

    FIX: se alinean logos empresas

--- src/sections/Customers.vue
+++ src/sections/Customers.vue
@@ -24,12 +24,12 @@
                     </div>
                     <div class="column is-3">
                         <a href="https://www.bhp.com/espanol" target="_blank">
-                            <img src="../assets/customers/bhpLogo.png" alt="BHP" height="50%" width="50%">
+                            <img src="../assets/customers/bhpLogo.png" alt="BHP">
                         </a>
                     </div>
                     <div class="column is-3">
                         <a href="https://mma.gob.cl/" target="_blank">
-                            <img src="../assets/customers/MmaChile.png" alt="Ministerio Medio Ambiente" height="30%" width="30%">
+                            <img src="../assets/customers/MmaChile.png" alt="Ministerio Medio Ambiente">
                         </a>
                     </div>
                 </div>
@@ -41,17 +41,17 @@
                     </div>
                     <div class="column is-3">
                         <a href="https://www.angloamerican-chile.cl/?sc_lang=es-ES" target="_blank">
-                            <img src="../assets/customers/anglo-american-logo.png" alt="anglo american" height="70%" width="70%">
+                            <img src="../assets/customers/anglo-american-logo.png" alt="anglo american">
                         </a>
                     </div>
                     <div class="column is-3">
                         <a href="http://www.jaimeillanes.cl/" target="_blank">
-                            <img src="../assets/customers/logo-jaime-illanes-asociados-1.png" alt="jaime illanes" height="70%" width="70%">
+                            <img src="../assets/customers/logo-jaime-illanes-asociados-1.png" alt="jaime illanes">
                         </a>
                     </div>
                     <div class="column is-3">
                         <a href="http://www.cenma.cl/" target="_blank">
-                            <img src="../assets/customers/cenma_logo.png" alt="Cenma" height="30%" width="30%">
+                            <img src="../assets/customers/cenma_logo.png" alt="Cenma">
                         </a>
                     </div>
                 </div>
@@ -63,73 +63,73 @@
                     </div>
                     <div class="column is-3">
                         <a href="https://www.bhp.com/espanol" target="_blank">
-                            <img src="../assets/customers/escondida2.png" alt="Escondida" height="40%" width="40%">
+                            <img src="../assets/customers/escondida2.png" alt="Escondida">
                         </a>
                     </div>
                     <div class="column is-3">
                         <a href="https://www.sqm.com/" target="_blank">
-                            <img src="../assets/customers/1200px-Sociedad_Química_y_Minera_logo.svg.png" alt="SQM" height="30%" width="30%">
+                            <img src="../assets/customers/1200px-Sociedad_Química_y_Minera_logo.svg.png" alt="SQM">
                         </a>
                     </div>
                     <div class="column is-3">
                         <a href="https://www.algoritmospa.com/" target="_blank">
-                            <img src="../assets/customers/algoritmos-logo.png" alt="Algoritmos" height="70%" width="70%">
+                            <img src="../assets/customers/algoritmos-logo.png" alt="Algoritmos">
                         </a>
                     </div>
                 </div>
                 <div class="columns">
                     <div class="column is-3">
                         <a href="https://www.sgs.cl/" target="_blank">
-                            <img src="../assets/customers/SGS_Logo.svg.png" alt="SGS" height="50%" width="50%">
+                            <img src="../assets/customers/SGS_Logo.svg.png" alt="SGS">
                         </a>
                     </div>
                     <div class="column is-3">
                         <a href="https://www.better.cl/" target="_blank">
-                            <img src="../assets/customers/better_logo.png" alt="Better" height="80%" width="80%" >
+                            <img src="../assets/customers/better_logo.png" alt="Better">
                         </a>
                     </div>
                     <div class="column is-3">
                         <a href="https://www.quimetal.cl/" target="_blank">
-                            <img src="../assets/customers/logoQuimetal.png" alt="Quimetal" height="40%" width="40%">
+                            <img src="../assets/customers/logoQuimetal.png" alt="Quimetal">
                         </a>
                     </div>
                     <div class="column is-3">
                         <a href="https://www.codelco.com/" target="_blank">
-                            <img src="../assets/customers/Codelco_logo.svg.png" alt="Codelco" height="38%" width="38%">
+                            <img src="../assets/customers/Codelco_logo.svg.png" alt="Codelco">
                         </a>
                     </div>
                 </div>
                 <div class="columns">
                     <div class="column is-3">
                         <a href="https://www.eye3.cl/" target="_blank">
-                            <img src="../assets/customers/logo_eye3.png" alt="Eye3" height="40%" width="40%">
+                            <img src="../assets/customers/logo_eye3.png" alt="Eye3">
                         </a>
                     </div>
                     <div class="column is-3">
                         <a href="https://www.enami.cl/" target="_blank">
-                            <img src="../assets/customers/enamiLogo.jpeg" alt="Enami" height="70%" width="70%">
+                            <img src="../assets/customers/enamiLogo.jpeg" alt="Enami">
                         </a>
                     </div>  
                     <div class="column is-3">
                         <a href="https://www.gob.pe/minam/oefa" target="_blank">
-                            <img src="../assets/customers/oefaLogo.png" alt="Oefa" height="60%" width="60%">
+                            <img src="../assets/customers/oefaLogo.png" alt="Oefa">
                         </a>
                     </div>
                     <div class="column is-3">
                         <a href="http://www.myma.cl/" target="_blank">
-                            <img src="../assets/customers/Myma_logo.png" alt="Myma" height="60%" width="60%">
+                            <img src="../assets/customers/Myma_logo.png" alt="Myma">
                         </a>
                     </div>
                 </div>
                 <div class="columns">
                     <div class="column is-3 is-offset-one-quarter">
                         <a href="https://www.anam.cl/inicio" target="_blank">
-                            <img src="../assets/customers/Anam_logo.jpg" alt="anam" height="70%" width="70%">
+                            <img src="../assets/customers/Anam_logo.jpg" alt="anam">
                         </a>
                     </div>
                     <div class="column is-3">
                         <a href="http://www.e-i.cl" target="_blank">
-                            <img src="../assets/customers/EILogo.png" alt="EI" height="60%" width="60%">
+                            <img src="../assets/customers/EILogo.png" alt="EI">
                         </a>
                     </div>
                 </div>
@@ -168,27 +168,27 @@
         padding-bottom: 1vh;
     }
 
-    .description{
+    .description {
         color: #8190A5;
         padding-left: 8vw;
         padding-right: 8vw;
         padding-bottom: 5vh;
     }
 
-    .color {
-        width: 100%;
-        height: 100%;
-        background-color: rgb(226, 226, 226);
-    }
-
     .column{
         height: 12vh;
+        /* border: 1px solid red; */
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
     }
 
-    .img{
-        margin-left: auto;
-        margin-right: auto;
+    .column img {
+        max-width: 100%;
+        max-height: 9vh;
     }
+
 
     @media (max-width: 1024px) {
         .hero {
