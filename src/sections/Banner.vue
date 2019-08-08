249d05ae9744ccd57bce1c750a61af150a8d6bf7
b-vera <bastian.vera.p@usach.cl>
2019-08-08 17:18:15 -0400

    CHG: se agregan imagenes al banner

--- src/sections/Banner.vue
+++ src/sections/Banner.vue
@@ -4,9 +4,21 @@
             <div class="columns" id="columns-header">
                 <div class="column is-full">
                     <div class="columns">
-                      <div class="column"> <CardBanner image='../assets/Banner/banner_1.jpg' title="Nuestro sello" description="Respondemos con calidad, puntualidad y compromiso en nuestro servicios."> </CardBanner> </div>
-                      <div class="column"> <CardBanner image='../assets/Banner/banner_2.jpg' title="Somos tu gran socio" description="Te apoyamos en tus estudios y declaraciones de impacto ambiental."> </CardBanner> </div>
-                      <div class="column"> <CardBanner image='../assets/Banner/banner_3.jpg' title="Garantía ecólogica" description="Te ayudamos a ser una empresa comprometida con el medio ambiente."> </CardBanner> </div>
+                        <div class="column">
+                            <CardBanner image='banner_1.jpg' title="Nuestro sello"
+                                description="Respondemos con calidad, puntualidad y compromiso en nuestro servicios.">
+                            </CardBanner>
+                        </div>
+                        <div class="column">
+                            <CardBanner image='banner_2.jpg' title="Somos tu gran socio"
+                                description="Te apoyamos en tus estudios y declaraciones de impacto ambiental.">
+                            </CardBanner>
+                        </div>
+                        <div class="column">
+                            <CardBanner image='banner_3.jpg' title="Garantía ecólogica"
+                                description="Te ayudamos a ser una empresa comprometida con el medio ambiente.">
+                            </CardBanner>
+                        </div>
                     </div>
                 </div>
             </div>
