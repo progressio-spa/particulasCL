40813d0c5831f7f6a5c1ab2b21ea50de4c407f6d
b-vera <bastian.vera.p@usach.cl>
2019-08-27 16:37:25 -0400

    CHG: Se alinea imagen con texto en vertical

--- src/sections/Training.vue
+++ src/sections/Training.vue
@@ -7,39 +7,34 @@
                         <p class="title">Capacitación</p>
                     </div>
                     <div class="description">
-                        <p>Según el Ministerio del Medio Ambiente de Chile, la educación ambiental es un instrumento de
+                        <p>
+                            Según el Ministerio del Medio Ambiente de Chile, la educación ambiental es un instrumento de
                             gestión cuyo propósito es educar a la ciudadanía para el desarrollo sustentable,
                             generando conciencia y cambios conductuales proclives hacia la convivencia armónica entre
                             el desarrollo social, crecimiento económico y cuidado del medio ambiente.
-                            <br>
-                            PARTICULAS Capacitación, área de formación y desarrollo, está conformada por un equipo
+                            <br />PARTICULAS Capacitación, área de formación y desarrollo, está conformada por un equipo
                             multidisciplinario de profesionales, académicos, investigadores, asesores y consultores
                             especializados en temas ambientales, los cuales cuentan con una vasta experiencia en el
                             ámbito de la capacitación impartida en sector público y privado.
-                            <br>
-                            Generamos valor en las personas, mejorando sus competencias a través de un proceso de
+                            <br />Generamos valor en las personas, mejorando sus competencias a través de un proceso de
                             enseñanza que privilegia el aprender haciendo.
-                            <br>
-                            Apoyamos a las instituciones en el diseño, implementación, evaluación y mejoramiento de las
+                            <br />Apoyamos a las instituciones en el diseño, implementación, evaluación y mejoramiento de las
                             competencias personales y organizacionales para aumentar su productividad, competitividad,
                             sustentabilidad ambiental, efectividad económica y social.
-                            <br>
-                            Diseñamos programas de capacitación personalizados, a partir de la interacción conjunta con
+                            <br />Diseñamos programas de capacitación personalizados, a partir de la interacción conjunta con
                             los clientes, considerando los recursos disponibles y la garantía de resultados planeados.
                             Nuestra misión es ser el mejor socio estratégico de nuestros clientes, agregando valor a sus
                             negocios a través de las personas.
-                            <br>
-                            En este contexto, nuestra empresa ofrece a las Instituciones Públicas, Empresas Privadas,
+                            <br />En este contexto, nuestra empresa ofrece a las Instituciones Públicas, Empresas Privadas,
                             ONGs y Sociedad Civil los siguientes cursos y talleres.
-
                         </p>
                     </div>
                 </div>
             </div>
             <div class="columns" id="columns-body">
-                <div class="column is-half" id="left">
+                <div class="column is-full" id="left">
                     <div class="aboutUs-body">
-                        <ul>
+                        <ul style="list-style-type:disc;">
                             <li>Fundamentos de la contaminación atmosférica</li>
                             <li>Exposición a la contaminación atmosférica y efectos en salud humana</li>
                             <li>Herramientas de gestión de la calidad del aire</li>
@@ -50,8 +45,10 @@
                         </ul>
                     </div>
                 </div>
-                <div class="column is-half" id="right">
-                    <img src="../assets/capacitacion.png" alt="capacitacion">
+            </div>
+            <div class="columns" id="columns-image">
+                <div class="column is-full" id="right">
+                    <img src="../assets/capacitacion.png" alt="capacitacion" />
                 </div>
             </div>
         </div>
@@ -59,75 +56,65 @@
 </template>
 
 <style scoped>
+.hero {
+    background-color: white;
+}
 
-    .hero {
-        background-color: white;
+.hero-body {
+    display: flex;
+    flex-direction: column;
+    padding-left: 8vw;
+    padding-right: 8vw;
+}
+
+#columns-body {
+    width: 100%;
+    height: 250px;
+}
+
+.title {
+    color: #47525e;
+}
+
+.header {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    padding-bottom: 5vh;
+}
+
+.description {
+    color: #8190a5;
+    padding-bottom: 2vh;
+    text-align: justify;
+}
+
+.aboutUs-body {
+    height: 100%;
+    color: #8190a5;
+    text-align: justify;
+}
+
+#columns-header {
+    display: block;
+}
+
+@media (max-width: 1024px) {
+    .hero-body {
+        padding-top: 10%;
+    }
+    #columns-body {
+        height: 200px;
+    }
+}
+
+@media (max-width: 767px) {
+    #columns-body {
+        height: 400px;
     }
 
     .hero-body {
-        display: flex;
-        flex-direction: column;
-        padding-left: 8vw;
-        padding-right: 8vw;
+        padding-top: 10%;
     }
-
-    #columns-body {
-        width: 100%;
-        height: 450px;
-    }
-
-    .title {
-        color: #47525E;
-    }
-
-    .header {
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        padding-bottom: 5vh;
-    }
-
-    .description{
-        color: #8190A5;
-        padding-bottom: 2vh;
-        text-align: justify;
-    }
-
-    .aboutUs-body{
-        height:100%;
-        color: #8190A5;
-        text-align: justify;
-    }
-
-    #left{
-        padding-left:0;
-    }
-
-    #right{
-        padding-right:0;
-    }
-
-    #columns-header {
-        display: block;
-    }
-
-    @media (max-width: 1024px) {
-        .hero-body {
-            padding-top: 10%;
-        }
-        #columns-body {
-            height: 200px;
-        }
-    }
-
-    @media (max-width: 767px) {
-
-        #columns-body {
-            height: 550px;
-        }
-
-        .hero-body {
-            padding-top: 10%;
-        }
-    }
+}
 </style>
