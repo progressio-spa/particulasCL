c34ce8f5260bf245f0ead75b70c82af75d438e51
b-vera <bastian.vera.p@usach.cl>
2019-09-08 20:48:18 -0300

    CHG: se corrige identación

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
-
-	
-
-  <div id="app" class="mainApp">
-    <router-view/>
-  </div>
+    <div id="app" class="mainApp">
+        <router-view />
+    </div>
 </template>
 
 <style>
-
 @import url('https://fonts.googleapis.com/css?family=Lato&display=swap');
 
 .mainApp {
-	font-family: 'Lato', sans-serif;
-	text-align: justify;
+    font-family: 'Lato', sans-serif;
+    text-align: justify;
 }
-
 </style>
