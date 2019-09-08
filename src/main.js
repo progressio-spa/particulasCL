e28fc9def1b28e08b1210c9ed67b135280a54b7b
b-vera <bastian.vera.p@usach.cl>
2019-09-08 20:49:32 -0300

    CHG: se agrega componente carrousel-bulma

--- src/main.js
+++ src/main.js
@@ -3,31 +3,32 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
-import "bulma/css/bulma.css"
-import "@fortawesome/fontawesome-free/css/all.css"
-import VueScrollTo from 'vue-scrollto';
+import 'bulma/css/bulma.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'bulma-carousel/dist/css/bulma-carousel.min.css'
+import VueScrollTo from 'vue-scrollto'
 
 Vue.use(VueScrollTo)
- 
+
 // You can also pass in the default options
 Vue.use(VueScrollTo, {
-     container: "body",
-     duration: 500,
-     easing: "ease",
-     offset: 1000,
-     force: true,
-     cancelable: true,
-     onStart: false,
-     onDone: false,
-     onCancel: false,
-     x: false,
-     y: true
- })
+    container: 'body',
+    duration: 500,
+    easing: 'ease',
+    offset: 1000,
+    force: true,
+    cancelable: true,
+    onStart: false,
+    onDone: false,
+    onCancel: false,
+    x: false,
+    y: true,
+})
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App),
 }).$mount('#app')
