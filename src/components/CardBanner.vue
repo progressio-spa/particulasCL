11cec5cfc8c86f4cf49d51f713523de37dd605fa
b-vera <bastian.vera.p@usach.cl>
2019-09-10 16:23:11 -0300

    CHG: se corrige identación

--- src/components/CardBanner.vue
+++ src/components/CardBanner.vue
@@ -1,42 +1,39 @@
 <template>
-	<div>
-		<div class="card">
-		  <div class="card-image">
-		    <figure class="image is-4by3">
-		      <img :src="require(`@/assets/Banner/${image}`)" alt="Placeholder image">
-		    </figure>
-		  </div>
-		  <div class="card-content">
-		    <div class="media">
-		      <div class="media-content">
-		        <p class="title is-4">{{title}}</p>
-		      </div>
-		    </div>
+    <div>
+        <div class="card">
+            <div class="card-image">
+                <figure class="image is-4by3">
+                    <img :src="require(`@/assets/Banner/${image}`)" alt="Placeholder image" />
+                </figure>
+            </div>
+            <div class="card-content">
+                <div class="media">
+                    <div class="media-content">
+                        <p class="title is-4">{{title}}</p>
+                    </div>
+                </div>
 
-		    <div class="content">
-		      {{description}}
-		      <br>
-		    </div>
-		  </div>
-		</div>
-	</div>
-
+                <div class="content">
+                    {{description}}
+                    <br />
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-	export default {
-		props: {
-			image: String,
-			title: String,
-			description: String
-		}
-	}
+export default {
+    props: {
+        image: String,
+        title: String,
+        description: String,
+    },
+}
 </script>
 
 <style scoped>
-
-	.card-content {
-	  height: 100%;
-	}
-	
+.card-content {
+    height: 100%;
+}
 </style>
