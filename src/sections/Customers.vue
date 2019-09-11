2f8b214f43bdfdaa5714a527178fd9072078306a
b-vera <bastian.vera.p@usach.cl>
2019-09-10 22:45:25 -0300

    CHG: se agrega carrusel a lista de clientes

--- src/sections/Customers.vue
+++ src/sections/Customers.vue
@@ -1,214 +1,254 @@
 <template>
-    <section class="hero is-fullheight" id="customers">
+    <section class="hero is-medium" id="customers">
         <div class="hero-body">
             <div class="header-description">
                 <div class="header">
                     <p class="title">Nuestros Clientes</p>
                 </div>
                 <div class="description">
-                    <p>Instituciones públicas y privadas
-                    </p>
+                    <p>Instituciones públicas y privadas</p>
                 </div>
             </div>
             <div class="columns-body">
-                <div class="columns">
-                    <div class="column is-3">
-                        <a href="https://www.golder.com/" target="_blank">
-                            <img src="../assets/customers/golderLogo.png" alt="golder">
-                        </a>
+                <div class="container">
+                    <!-- Start Carousel -->
+                    <div id="carousel-demo" class="carousel">
+                        <div class="item">
+                            <a href="https://www.golder.com/" target="_blank">
+                                <img src="../assets/customers/golderLogo.png" alt="golder" />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a
+                                href="https://www.angloamerican-chile.cl/?sc_lang=es-ES"
+                                target="_blank"
+                            >
+                                <img
+                                    src="../assets/customers/anglo-american-logo.png"
+                                    alt="anglo american"
+                                />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://www.bhp.com/espanol" target="_blank">
+                                <img src="../assets/customers/bhpLogo.png" alt="BHP" />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://mma.gob.cl/" target="_blank">
+                                <img
+                                    src="../assets/customers/MmaChile.png"
+                                    alt="Ministerio Medio Ambiente"
+                                />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://www.arcadis.com/es-cl/chile/" target="_blank">
+                                <img src="../assets/customers/arcadis-logo.png" alt="Arcadis" />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://www.enap.cl/" target="_blank">
+                                <img src="../assets/customers/logo_enap.gif" alt="enap" />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="http://www.jaimeillanes.cl/" target="_blank">
+                                <img
+                                    src="../assets/customers/logo-jaime-illanes-asociados-1.png"
+                                    alt="jaime illanes"
+                                />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="http://www.cenma.cl/" target="_blank">
+                                <img src="../assets/customers/cenma_logo.png" alt="Cenma" />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://www.usm.cl/" target="_blank">
+                                <img src="../assets/customers/header-usm.png" alt="USM" />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://www.bhp.com/espanol" target="_blank">
+                                <img src="../assets/customers/escondida2.png" alt="Escondida" />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://www.sqm.com/" target="_blank">
+                                <img
+                                    src="../assets/customers/1200px-Sociedad_Química_y_Minera_logo.svg.png"
+                                    alt="SQM"
+                                />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://www.algoritmospa.com/" target="_blank">
+                                <img src="../assets/customers/algoritmos-logo.png" alt="Algoritmos" />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://www.sgs.cl/" target="_blank">
+                                <img src="../assets/customers/SGS_Logo.svg.png" alt="SGS" />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://www.better.cl/" target="_blank">
+                                <img src="../assets/customers/better_logo.png" alt="Better" />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://www.quimetal.cl/" target="_blank">
+                                <img src="../assets/customers/logoQuimetal.png" alt="Quimetal" />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://www.codelco.com/" target="_blank">
+                                <img src="../assets/customers/Codelco_logo.svg.png" alt="Codelco" />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://www.eye3.cl/" target="_blank">
+                                <img src="../assets/customers/logo_eye3.png" alt="Eye3" />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://www.enami.cl/" target="_blank">
+                                <img src="../assets/customers/enamiLogo.jpeg" alt="Enami" />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://www.gob.pe/minam/oefa" target="_blank">
+                                <img src="../assets/customers/oefaLogo.png" alt="Oefa" />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="http://www.myma.cl/" target="_blank">
+                                <img src="../assets/customers/Myma_logo.png" alt="Myma" />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://www.anam.cl/inicio" target="_blank">
+                                <img src="../assets/customers/Anam_logo.jpg" alt="anam" />
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="http://www.e-i.cl" target="_blank">
+                                <img src="../assets/customers/EILogo.png" alt="EI" />
+                            </a>
+                        </div>
                     </div>
-                    <div class="column is-3">
-                        <a href="https://www.enap.cl/" target="_blank">
-                            <img src="../assets/customers/logo_enap.gif" alt="enap">
-                        </a>
-                    </div>
-                    <div class="column is-3">
-                        <a href="https://www.bhp.com/espanol" target="_blank">
-                            <img src="../assets/customers/bhpLogo.png" alt="BHP">
-                        </a>
-                    </div>
-                    <div class="column is-3">
-                        <a href="https://mma.gob.cl/" target="_blank">
-                            <img src="../assets/customers/MmaChile.png" alt="Ministerio Medio Ambiente">
-                        </a>
-                    </div>
-                </div>
-                <div class="columns">
-                    <div class="column is-3">
-                        <a href="https://www.arcadis.com/es-cl/chile/" target="_blank">
-                            <img src="../assets/customers/arcadis-logo.png" alt="Arcadis">
-                        </a>
-                    </div>
-                    <div class="column is-3">
-                        <a href="https://www.angloamerican-chile.cl/?sc_lang=es-ES" target="_blank">
-                            <img src="../assets/customers/anglo-american-logo.png" alt="anglo american">
-                        </a>
-                    </div>
-                    <div class="column is-3">
-                        <a href="http://www.jaimeillanes.cl/" target="_blank">
-                            <img src="../assets/customers/logo-jaime-illanes-asociados-1.png" alt="jaime illanes">
-                        </a>
-                    </div>
-                    <div class="column is-3">
-                        <a href="http://www.cenma.cl/" target="_blank">
-                            <img src="../assets/customers/cenma_logo.png" alt="Cenma">
-                        </a>
-                    </div>
-                </div>
-                <div class="columns">
-                    <div class="column is-3">
-                        <a href="https://www.usm.cl/" target="_blank">
-                            <img src="../assets/customers/header-usm.png" alt="USM">
-                        </a>
-                    </div>
-                    <div class="column is-3">
-                        <a href="https://www.bhp.com/espanol" target="_blank">
-                            <img src="../assets/customers/escondida2.png" alt="Escondida">
-                        </a>
-                    </div>
-                    <div class="column is-3">
-                        <a href="https://www.sqm.com/" target="_blank">
-                            <img src="../assets/customers/1200px-Sociedad_Química_y_Minera_logo.svg.png" alt="SQM">
-                        </a>
-                    </div>
-                    <div class="column is-3">
-                        <a href="https://www.algoritmospa.com/" target="_blank">
-                            <img src="../assets/customers/algoritmos-logo.png" alt="Algoritmos">
-                        </a>
-                    </div>
-                </div>
-                <div class="columns">
-                    <div class="column is-3">
-                        <a href="https://www.sgs.cl/" target="_blank">
-                            <img src="../assets/customers/SGS_Logo.svg.png" alt="SGS">
-                        </a>
-                    </div>
-                    <div class="column is-3">
-                        <a href="https://www.better.cl/" target="_blank">
-                            <img src="../assets/customers/better_logo.png" alt="Better">
-                        </a>
-                    </div>
-                    <div class="column is-3">
-                        <a href="https://www.quimetal.cl/" target="_blank">
-                            <img src="../assets/customers/logoQuimetal.png" alt="Quimetal">
-                        </a>
-                    </div>
-                    <div class="column is-3">
-                        <a href="https://www.codelco.com/" target="_blank">
-                            <img src="../assets/customers/Codelco_logo.svg.png" alt="Codelco">
-                        </a>
-                    </div>
-                </div>
-                <div class="columns">
-                    <div class="column is-3">
-                        <a href="https://www.eye3.cl/" target="_blank">
-                            <img src="../assets/customers/logo_eye3.png" alt="Eye3">
-                        </a>
-                    </div>
-                    <div class="column is-3">
-                        <a href="https://www.enami.cl/" target="_blank">
-                            <img src="../assets/customers/enamiLogo.jpeg" alt="Enami">
-                        </a>
-                    </div>  
-                    <div class="column is-3">
-                        <a href="https://www.gob.pe/minam/oefa" target="_blank">
-                            <img src="../assets/customers/oefaLogo.png" alt="Oefa">
-                        </a>
-                    </div>
-                    <div class="column is-3">
-                        <a href="http://www.myma.cl/" target="_blank">
-                            <img src="../assets/customers/Myma_logo.png" alt="Myma">
-                        </a>
-                    </div>
-                </div>
-                <div class="columns">
-                    <div class="column is-3 is-offset-one-quarter">
-                        <a href="https://www.anam.cl/inicio" target="_blank">
-                            <img src="../assets/customers/Anam_logo.jpg" alt="anam">
-                        </a>
-                    </div>
-                    <div class="column is-3">
-                        <a href="http://www.e-i.cl" target="_blank">
-                            <img src="../assets/customers/EILogo.png" alt="EI">
-                        </a>
-                    </div>
+                    <!-- End Carousel -->
                 </div>
             </div>
         </div>
     </section>
 </template>
 
+<script>
+import bulmaCarousel from 'bulma-carousel/dist/js/bulma-carousel.min.js'
+
+export default {
+    components: {
+        bulmaCarousel,
+    },
+    data() {
+        return {
+            user: this.user,
+        }
+    },
+    mounted() {
+        bulmaCarousel.attach('#carousel-demo', {
+            slidesToScroll: 1,
+            slidesToShow: 4,
+            infinite: true,
+            initialSlide: 10,
+            breakpoints: [
+                { changePoint: 768, slidesToShow: 1, slidesToScroll: 1 },
+            ],
+        })
+    },
+}
+</script>
+
 <style scoped>
-    .hero{
-        background: white url('../assets/DegradeBlanco.svg');
-        background-size: cover;
+.hero {
+    background: white url('../assets/DegradeBlanco.svg');
+    background-size: cover;
+}
+.hero-body {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-top: 5%;
+    padding-left: 8vw;
+    padding-right: 8vw;
+}
+
+.columns-body {
+    width: 100%;
+    height: 200px;
+}
+
+.title {
+    color: #47525e;
+}
+
+.header {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    padding-bottom: 1vh;
+}
+
+.description {
+    color: #8190a5;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+
+.item {
+    height: 30vh;
+    /* border: 1px solid red; */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.item img {
+    max-width: 100%;
+    max-height: 10vh;
+}
+
+#carousel-demo {
+    overflow-x: hidden;
+}
+
+@media (max-width: 1024px) {
+    .hero {
+        background: white;
     }
     .hero-body {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        padding-top: 5%;
-        padding-left: 8vw;
-        padding-right: 8vw;
+        padding-top: 0%;
     }
-
     .columns-body {
-        width: 100%;
-        height: 600px;
+        height: 400px;
+    }
+}
+
+@media (max-width: 767px) {
+    .hero {
+        background: white;
     }
 
-    .title {
-        color: #47525E;
+    .hero-body {
+        padding-top: 10%;
     }
-
-    .header {
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        padding-bottom: 1vh;
+    .columns-body {
+        height: 200px;
     }
-
-    .description {
-        color: #8190A5;
-        padding-left: 8vw;
-        padding-right: 8vw;
-        padding-bottom: 5vh;
-    }
-
-    .column{
-        height: 12vh;
-        /* border: 1px solid red; */
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .column img {
-        max-width: 100%;
-        max-height: 9vh;
-    }
-
-
-    @media (max-width: 1024px) {
-        .hero {
-            background: white;
-        }        
-        .hero-body {
-            padding-top: 0%;
-        }
-    }
-
-    @media (max-width:  767px) {
-        .hero {
-            background: white;
-        }
-            
-        .hero-body {
-            padding-top: 10%;
-        }
-        .columns-body {
-            height: 2200px;
-        }
-    }
+}
 </style>
